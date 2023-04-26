57cc6331146da38f60cced10ae20079bac1568c8
KoalaTreeHotel <125790817+KoalaTreeHotel@users.noreply.github.com>
2023-04-26 12:56:19 +0100

    initial commit and challenge complete

---
+++ challenge-to-do.md
@@ -0,0 +1,8 @@
+1: Create an array of your favourite films / TV shows, up to 5 items. Use an array method to add 2 more items to your array. Use a loop to cycle through the array and log each item to the console.
+
+2: Generate 10 random numbers between 1-100 and log them to the console.
+
+3: Create a loop that counts backwards from 20-0.
+
+4: Generate 5 random numbers between 1-50. For each number generated, check if the number is divisible by 5 or not. Log whether it is divisible or not to the console.
+
---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+console.log("---------- (1) Array films");
+let myArr = ["IT", "ET", "10", "Ted", "Up"];
+myArr.push("Top Gun");
+myArr.push("Star Wars");
+for (let i = 0; i < myArr.length; i++) {
+    console.log(myArr[i]);
+}
+
+console.log("---------- (2) random numbers to array");
+let rangeLow = 1;
+let rangeHigh = 100;
+for (let i = 0; i < 10; i++) {
+    let randNo = Math.floor(Math.random() * (rangeHigh - rangeLow + 1)) + rangeLow;
+    console.log(randNo);
+}
+
+console.log("---------- (3) count backwards from 20 to 0");
+for (let i=20; i>=0; i--) {
+    console.log(i);
+}
+
+console.log("---------- (4) generate 5 random numbers in range 1-50, report if number divisible by 5");
+rangeLow = 1;
+rangeHigh = 50;
+for (let i = 0; i < 5; i++) {
+    let randNo = Math.floor(Math.random() * (rangeHigh - rangeLow + 1)) + rangeLow;
+    let divisibleBy5 = randNo%5===0 ? true : false;
+    if (divisibleBy5===true) {
+        console.log(randNo+": is divisible by 5");
+    }
+    else {
+        console.log(randNo+": is not divisible by 5");
+    }
+}
