bcbc763bf6ec036ad8731e44c7829147158b360e
Stew Fraser <stewart.fraser@beatzero.co.uk>
2024-09-22 07:53:27 +0100

    combining assignments: variables (adding code)

---
+++ script-variables.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let myName = "John Doe";
+console.log("My name is: " + myName + ".\n");
+
+let favCol = "blue";
+let favFilm = "Star Wars";
+console.log(`My name is ${myName}.
+My favourite film is ${favFilm}.
+My favourite colour is ${favCol}.\n`);
+
+let breakfast = "eggs";
+let lunch = "lentil soup";
+let dinner = "pizza";
+console.log(`For breakfast, I ate ${breakfast}.
+For lunch I ate ${lunch}.
+For dinner I ate ${dinner}.`);
+console.log("----- reassign variables");
+breakfast = "toast";
+lunch = "spag bol";
+dinner = "roast chicken";
+console.log(`For breakfast, I ate ${breakfast}.
+For lunch I ate ${lunch}.
+For dinner I ate ${dinner}.`);
